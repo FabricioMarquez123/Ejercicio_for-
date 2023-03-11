34940ae57bbd44876419406e015bca6c3ecac5fd
FabricioMarquez123 <114788319+FabricioMarquez123@users.noreply.github.com>
2023-03-11 10:23:31 -0600

    Add files via upload

---
+++ ejericio for/Program.cs
@@ -0,0 +1,17 @@
+﻿class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese un número entero:");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= num; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
